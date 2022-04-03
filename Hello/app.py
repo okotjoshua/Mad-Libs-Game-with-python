499b09e3bbfdf7e38f4ff885e279af9b7e1fac66
okotjoshua <okotjoshua43@gmail.com>
2022-04-03 21:58:15 +0300

    Update app.py

--- Hello/app.py
+++ Hello/app.py
@@ -1,7 +1,7 @@
 color = input("Enter a color: ")
-plural_noun = input("Enter any noun: ")
+noun = input("Enter any noun: ")
 celebrity = input("Enter your celebrity name: ")
 
 print("Roses are " + color)
-print(plural_noun + " are black")
+print(noun + " are black")
 print("I love " + celebrity)
